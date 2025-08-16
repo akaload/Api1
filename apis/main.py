7334a367c21ead9a58a119ae8b092a2bfc3f4fb8
Jacobo Loaiza Cano <akaload158@gmail.com>
2025-08-16 11:15:50 -0500

    Feat: cree el main con las apis rest del get

---
+++ apis/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
+
+@app.get("/saludo/{nombre}")
+def get_saludo(nombre: str):
+    return {"message": f"Hola {nombre}"}
+
